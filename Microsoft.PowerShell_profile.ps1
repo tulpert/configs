9f9478da6c86fa7ac7777a35c28d00c4f9b71a1c
Tulpert <tulpert@outlook.com>
2017-08-04 20:01:49 +0200

    Added config for Powershell. Check Path with C:\Users\tulpert\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Chord Ctrl+LeftArrow -Function BackwardWord
+Set-PSReadlineKeyHandler -Chord Ctrl+RightArrow -Function NextWord
+Set-PSReadlineKeyHandler -Chord Ctrl+Shift+LeftArrow -Function SelectBackwardWord
+Set-PSReadlineKeyHandler -Chord Ctrl+Shift+RightArrow -Function SelectNextWord
+Set-PSReadlineKeyHandler -Chord Ctrl+Backspace -Function BackwardKillWord
